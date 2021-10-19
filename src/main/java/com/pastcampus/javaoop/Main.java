f3478021ef88aab56a1978848c500cab50fd767b
Son GwangJin <start_so@naver.com>
2021-10-20 01:23:56 +0900

    버블 소트 , 자바 Collections.sort 테스트

--- src/main/java/com/pastcampus/javaoop/Main.java
+++ src/main/java/com/pastcampus/javaoop/Main.java
@@ -1,7 +1,14 @@
 package com.pastcampus.javaoop;
 
+import com.pastcampus.javaoop.logic.BubbleSort;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("hello World");
+        BubbleSort<String> sort = new BubbleSort<>();
+
+        System.out.println("[result]" + sort.sort(Arrays.asList(args)));
     }
 }
